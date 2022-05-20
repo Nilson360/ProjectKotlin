c00c130cbaf54863b469699494e435716199c8d2
Elowan <elowan.engel@ecole-hexagone.com>
2022-05-20 10:48:39 +0200

    Navigation reussie

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,6 +1,32 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import com.example.myapplication.databinding.HomeLayoutBinding
+import com.example.myapplication.databinding.SplashLayoutBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment () {
+
+    private lateinit var binding: HomeLayoutBinding
+    private val navArgs : HomeFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = HomeLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvTitle.text = getString(R.string.bonjour_s_et_bienvenue_sur_l_app, navArgs.name)
+    }
 }
--- app/src/main/java/com/example/myapplication/SplashFragment.kt
+++ app/src/main/java/com/example/myapplication/SplashFragment.kt
@@ -1,7 +1,31 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.SplashLayoutBinding
 
+class SplashFragment : Fragment() {
 
-class SplashFragment : Fragment(){
+    private lateinit var binding: SplashLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = SplashLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mbNavButton.setOnClickListener{
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment( "Anicet"))
+        }
+    }
 }
