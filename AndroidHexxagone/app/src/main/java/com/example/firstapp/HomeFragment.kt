a70baef0b690f660ebdf3087a5886efb7776df95
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2022-05-20 09:15:26 +0200

    Create screen and working with design in Android Studio

--- AndroidHexxagone/app/src/main/java/com/example/firstapp/HomeFragment.kt
+++ AndroidHexxagone/app/src/main/java/com/example/firstapp/HomeFragment.kt
@@ -1,6 +1,27 @@
 package com.example.firstapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.firstapp.databinding.HomeLayoutBinding
+import com.example.firstapp.databinding.SplashLayoutBinding
 
 class HomeFragment:Fragment() {
+    private lateinit var binding: HomeLayoutBinding
+    private val navArgs: HomeFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = HomeLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.textView.text = getString(R.string.bienvenue_dans_votre_espaca_personnel, navArgs.name)
+    }
 }
--- AndroidHexxagone/app/src/main/java/com/example/firstapp/SplashFragment.kt
+++ AndroidHexxagone/app/src/main/java/com/example/firstapp/SplashFragment.kt
@@ -1,6 +1,29 @@
 package com.example.firstapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.firstapp.databinding.SplashLayoutBinding
 
 class SplashFragment:Fragment() {
+    private lateinit var binding: SplashLayoutBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =  SplashLayoutBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btn1.setOnClickListener {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment2(name = "Coisa doida"))
+        }
+
+    }
 }
